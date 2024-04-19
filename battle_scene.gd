1adacf6b73cdcc82cf5d0ba9471f335d91b329ed
Jose Ruiz <jruiz170.business@gmail.com>
2024-04-18 23:54:45 -0400

    Adding Enemy turn and attack functionality with animations

--- battle_scene.gd
+++ battle_scene.gd
@@ -2,12 +2,24 @@ extends Control
 
 signal pressedEnter
 
+@export var enemy : Resource = null
+
+var currentPlayerHealth
+var currentEnemyHealth
+
 func _ready():
+	setHealth($EnemyContainer/EnemyHealth, enemy.health, enemy.health)
 	setHealth($PlayerHUD/PlayerData/PlayerHealth, State.currentHealth, State.maxHealth)
-	setHealth($EnemyContainer/EnemyHealth, 20, 20)
+	$EnemyContainer/EnemySprite.texture = enemy.texture
 	$BattleText.hide()
 	$PlayerActionsHUD.hide()
-	display_text("Oh man, here comes this thing again. When will he learn?")
+	currentPlayerHealth = State.currentHealth
+	currentEnemyHealth = enemy.health
+	display_text("Oh man, here comes %s again. When will he learn?" % [enemy.name])
+	await pressedEnter
+	tickerSwitch(false)
+	$BattleText.hide()
+	$PlayerActionsHUD.show()
 
 func _process(delta):
 	pass
@@ -17,13 +29,16 @@ func display_text(text):
 	$BattleText.show()
 	tickerSwitch(true)
 	$BattleText/Label.text = text
-	await pressedEnter
-	tickerSwitch(false)
-	$BattleText.hide()
-	$PlayerActionsHUD.show()
+
+func tickerSwitch(switch : bool):
+	if switch:
+		$FlickerTicker.start()
+	else:
+		$FlickerTicker.stop()
+		$BattleText/Ticker.visible = false	
 
 func run():
-	display_text("You are a coward. A cretin.\nYou may have run from this battle, but you can never run from the eyes of He who judges all.")
+	display_text("You are a coward. A cretin.\nYou may have run from %s, but you can never run from the eyes of He who judges all." % [enemy.name]) 
 	await pressedEnter
 	get_tree().quit()
 
@@ -32,22 +47,37 @@ func setHealth(progress_bar, currentHealth, maxHealth):
 	progress_bar.max_value = maxHealth
 	progress_bar.get_node("Label").text = "HP: %d / %d" % [currentHealth, maxHealth]
 	
-func tickerSwitch(switch : bool):
-	if switch:
-		$FlickerTicker.start()
-	else:
-		$FlickerTicker.stop()
-		$BattleText/Ticker.visible = false
+func enemyTurn():
+	display_text("The %s attacks with all their might!" % [enemy.name])
+	$AnimationPlayer.play("enemy_attack")
+	await $AnimationPlayer.animation_finished
+	await pressedEnter
+	display_text("The %s dealt %s damage to you!" % [enemy.name, enemy.damage])
+	await pressedEnter
+	currentPlayerHealth = max(0, currentPlayerHealth - enemy.damage)
+	setHealth($PlayerHUD/PlayerData/PlayerHealth, currentPlayerHealth, State.maxHealth)
+	$BattleText.hide()
+	$PlayerActionsHUD.show()
+	
 
 func _input(event):
-	if event.is_action_pressed("ui_select") || event.is_action_pressed("LeftMouse"):
+	if event.is_action_pressed("ui_select") || event.is_action_released("LeftMouse"):
 		pressedEnter.emit()
 
 func _on_flicker_ticker_timeout():
 	$BattleText/Ticker.visible = !($BattleText/Ticker.visible)
-	
-
-
 
 func _on_run_pressed():
 	run()
+
+func _on_attack_pressed():
+	display_text("You attacked the %s." % [enemy.name])
+	await pressedEnter
+	currentEnemyHealth =  max(0, currentEnemyHealth - State.playerDamage)
+	setHealth($EnemyContainer/EnemyHealth, currentEnemyHealth, enemy.health)
+	$AnimationPlayer.play("enemy_damaged")
+	display_text("You dealt %s damage to the %s!" % [State.playerDamage, enemy.name])
+	await $AnimationPlayer.animation_finished
+	await pressedEnter
+	tickerSwitch(false)
+	enemyTurn()
